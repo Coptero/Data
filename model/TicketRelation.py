1fcf25e1610a29c6978b1255305c1ac68f4a2b34
gonzalo.serrano.gonzalez <gonzalo.serrano.gonzalez@alumnos.upm.es>
2020-02-04 13:31:02 +0100

    New validationSchema

--- model/TicketRelation.py
+++ model/TicketRelation.py
@@ -4,20 +4,20 @@ from pyspark.sql.types import StringType, StructField, StructType
 
 
 def getRelationSchema(spark):
-    ticket_id = [StructField("ticket_id", StringType(), True)]
-    ticket_type = [StructField("ticket_type", StringType(), True)]
-    related_ticket_id = [StructField("related_ticket_id", StringType(), True)]
-    related_ticket_type = [StructField("related_ticket_type", StringType(), True)]
-    association_type = [StructField("association_type", StringType(), True)]
-    submit_date = [StructField("submit_date", StringType(), True)]
-    relation_summary = [StructField("relation_summary", StringType(), True)]
-    status = [StructField("status", StringType(), True)]
-    submitter = [StructField("submitter", StringType(), True)]
-    instanceid = [StructField("instanceid", StringType(), True)]
-    corrupt_file_schema = [StructField("_corrupt_record", StringType(), True)]
-    validationSchema = StructType(ticket_id + ticket_type + related_ticket_id + related_ticket_type +
-                                  association_type + submit_date + relation_summary + status +
-                                  submitter + instanceid + corrupt_file_schema)
+    validationSchema = StructType([
+        StructField("ticket_id", StringType(), True),
+        StructField("ticket_type", StringType(), True),
+        StructField("related_ticket_id", StringType(), True),
+        StructField("related_ticket_type", StringType(), True),
+        StructField("association_type", StringType(), True),
+        StructField("submit_date", StringType(), True),
+        StructField("relation_summary", StringType(), True),
+        StructField("status", StringType(), True),
+        StructField("submitter", StringType(), True),
+        StructField("instanceid", StringType(), True),
+        StructField("_corrupt_record", StringType(), True)
+    ])
+
     return validationSchema
 
 
