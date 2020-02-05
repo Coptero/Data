d4fb6d6737a509fc87e414ca7e40c0d9a94a465f
gonzalo.serrano.gonzalez <gonzalo.serrano.gonzalez@alumnos.upm.es>
2020-02-05 13:21:27 +0100

    New Alert

--- Dsl/AlertsDsl.py
+++ Dsl/AlertsDsl.py
@@ -1,20 +1,29 @@
 # Faltan imports
 import logging
+from pyspark.sql import *
+from datetime import datetime
 from pyspark import *
+from model.Alert import Alert
+from elasticsearch import Elasticsearch
 
 
 class AlertDsl(logging):
     # Falta el implicit spark SparkSession
     def checkCount(indexName, fileName, dfCount, spark):
         logging.info('dfCount.. ' + dfCount)
-        path = fileName.replace(":", "\\:").replace("/", "\\/")
-        qResultDF = spark.esDF("${indexName}", "?q=file:\"" + path + "\"").select("ticket_id")
+        path = fileName.replace(':', r'\\:').replace("/", r"\\/")
+        qResultDF = spark.sqlContext.read \
+            .option("es.resource", "indexName") \
+            .set("es.query", r"?q=file:\'' + path + '\'' ") \
+            .format("org.elasticsearch.spark.sql") \
+            .load()
+        # ¿Equivalente de qResultDF = spark.esDF("${indexName}", "?q=file:\"" + path + "\"").select("ticket_id") ?
         qResultDF.cache
         queryCount = qResultDF.count
         qResultDF.unpersist
-
         logging.info("queryCount.. " + queryCount)
-
         if dfCount != queryCount:
-            alertDataFrame =  spark.Seq(Alert(fileName, dfCount, queryCount, new SimpleDateFormat("yyyyMMddHHmmss").format(Calendar.getInstance.getTime))).toDF()
+            now = datetime.now()
+            current_time = now.strftime("%Y%m%d%H%M%S")
+            alertDataFrame = spark.createDataFrame([Alert(fileName, dfCount, queryCount, current_time)])
             writeESAlertsIndex(alertDataFrame)
