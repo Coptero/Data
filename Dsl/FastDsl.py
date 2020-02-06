200b7bfa5ad3e1c7f2e11ebb8c03611746e13ebf
GalexMP <alexmedina907@gmail.com>
2020-02-06 11:26:06 +0100

    Files FastDsl.py and DynamoDBDsl.py modified, added class

--- Dsl/FastDsl.py
+++ Dsl/FastDsl.py
@@ -7,29 +7,29 @@ import model.FastPrincipal
 import model.FastServiceCircuit
 
 
-def postgreSQLAdminNumber(url, query, user, password, spark):
-    jdbcDF = postgreSQL(url, query, user, password, spark)
-    return fastColumns(jdbcDF)
+class FastDsl:
 
+    def postgreSQLAdminNumber(url, query, user, password, spark):
+        jdbcDF = postgreSQL(url, query, user, password, spark)
+        return fastColumns(jdbcDF)
 
-def postgreSQL(url, query, user, password, spark):
-    return spark.read\
-        .format("jdbc")\
-        .option("url", url)\
-        .option("user", user)\
-        .option("password", password)\
-        .option("query", query)\
-        .load()
+    def postgreSQLMain(url, query, user, password, spark):
+        jdbcDF = readFile("s3://rpajares/scalaSparkJobs/exportQueryFASTprincipal.csv")
+        return principalColumns(jdbcDF)
 
-def postgreSQLMain(url, query, user,password, spark):
-    jdbcDF = readFile("s3://rpajares/scalaSparkJobs/exportQueryFASTprincipal.csv")
-    return principalColumns(jdbcDF)
+    def postgreSQLServiceCircuit(url, query, user, password, spark):
+        jdbcDF = postgreSQL(url, query, user, password, spark)
+        return scColumns(jdbcDF)
 
+    def postgreSQLAggregated(url, query, user, password, spark):
+        jdbcDF = postgreSQL(url, query, user, password, spark)
+        return nnil2Columns(jdbcDF)
 
-def postgreSQLServiceCircuit(url, query, user,password, spark):
-    jdbcDF = postgreSQL(url, query, user, password, spark)
-    return scColumns(jdbcDF)
-
-def postgreSQLAggregated(url, query, user, password, spark):
-    jdbcDF = postgreSQL(url, query, user, password, spark)
-    return nnil2Columns(jdbcDF)
+    def postgreSQL(url, query, user, password, spark):
+        return spark.read \
+            .format("jdbc") \
+            .option("url", url) \
+            .option("user", user) \
+            .option("password", password) \
+            .option("query", query) \
+            .load()
