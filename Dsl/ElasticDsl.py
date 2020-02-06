8e89abd395b0a9d61137256f328af053323d4a63
gonzalo.serrano.gonzalez <gonzalo.serrano.gonzalez@alumnos.upm.es>
2020-02-06 11:51:04 +0100

    ElasticDsl corrected and IODsl created

--- Dsl/ElasticDsl.py
+++ Dsl/ElasticDsl.py
@@ -13,30 +13,28 @@ class ElasticDsl:
         ).save()
 
     def writeESCorruptRecordsIndex(index, name):
-      index.write.format(
-        'org.elasticsearch.spark.sql'
-      ).option(
-        'es.write.operation', 'index'
-      ).option(
-        'es.resource', name + datetime.now().strftime("%Y%m")
-      ).save()
+        index.write.format(
+            'org.elasticsearch.spark.sql'
+        ).option(
+            'es.write.operation', 'index'
+        ).option(
+            'es.resource', name + datetime.now().strftime("%Y%m")
+        ).save()
 
     def writeESAlertsIndex(index):
-      index.write.format(
-        'org.elasticsearch.spark.sql'
-      ).option(
-        'es.write.operation', 'index'
-      ).option(
-        'es.resource', 'copt-rod-alerts'
-      ).save()
+        index.write.format(
+            'org.elasticsearch.spark.sql'
+        ).option(
+            'es.write.operation', 'index'
+        ).option(
+            'es.resource', 'copt-rod-alerts'
+        ).save()
 
     def writeMappedESIndex(index, name, mapId):
-      index.write.format(
-        'org.elasticsearch.spark.sql'
-      ).option(
-        'es.mapping.id',  mapId
-      ).option(
-        'es.resource', name
-      ).save()
-
-
+        index.write.format(
+            'org.elasticsearch.spark.sql'
+        ).option(
+            'es.mapping.id', mapId
+        ).option(
+            'es.resource', name
+        ).save()
---
+++ Dsl/IODsl.py
@@ -0,0 +1,23 @@
+from pyspark.sql import *
+
+
+class IODsl:
+    '''
+      /**
+        * Read table from Hive
+        *
+        * @param table hive table
+        * @param spark SparkSession
+        * @return Dataset of ..
+        */
+    '''
+
+    def readTableHive(table, spark):
+        return spark.sqlContext.read.table(table.getTableName)
+
+
+class IOCopteroInsert:
+    def writeToHive(df, table, spark):
+        fields =  df.spark.read.table(table.getTableName).schema.fieldNames
+ #La linea da error desconocido df.select(fields.head, fields.tail:_*).write.mode(SaveMode.Overwrite).insertInto(table.getTableName)
+ #Falta por resolver como tratar los objetos, las clases y sus parametros ¿usar singleton?
