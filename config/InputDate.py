b81b08e9856328d6e29f2bd5d0116a1fbce949aa
GalexMP <alexmedina907@gmail.com>
2020-02-13 13:28:26 +0100

    file InputDate modified, change all methods

--- config/InputDate.py
+++ config/InputDate.py
@@ -1,58 +1,16 @@
-'''import org.joda.time.LocalDate
-import org.joda.time.format.DateTimeFormat
+from datetime import datetime, timedelta
 
-case class InputDate(year: String, month: String, day: String) {
-
-  lazy val Pattern = DateTimeFormat.forPattern("yyyyMMdd")
-
-  def getStringInputDate: String = {
-    s"$year$month$day"
-  }
-
-  def getLocalInputDate: LocalDate = {
-    Pattern.parseLocalDate(getStringInputDate)
-  }
-
-  def getStartDate(daysMinus: Int): LocalDate ={
-    getLocalInputDate.minusDays(daysMinus)
-  }
-
-
-}
-from functools import wraps
-
-def memoize(f):
-    @wraps(f)
-    def memoized(*args, **kwargs):
-        key = (args, tuple(sorted(kwargs.items()))) # make args hashable
-        result = memoized._cache.get(key, None)
-        if result is None:
-            result = f(*args, **kwargs)
-            memoized._cache[key] = result
-        return result
-    memoized._cache = {}
-    return memoized
-'''
-
-from dataclasses import dataclass
-import datetime
-
-@dataclass
-class ConfigJson(object):
+class InputDate(object):
     year: str
     month: str
     day: str
 
-    @property
-    @ memoize
-    def my_lazy_val(self):
-        return DateTimeFormat.forPattern("yyyyMMdd")
+    def getStringInputDate(self):
+        return self.year+self.month+self.day
 
+    def getLocalInputDate(self):
+        return  datetime.strptime(self.getStringInputDate, "%Y%m%d")
 
-  def getStringInputDate() :
-    return "year,month,day" #return strftime("%Y%m%d")
-
-  def getLocalInputDate() :
-    return getLocalInputDate.strftime("%Y%m")
-
-#¿
+    def getStartDate(self, daysMinus):
+        delta = datetime(daysMinus)
+        return self.getLocalInputDate() - delta
