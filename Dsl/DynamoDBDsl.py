465fa11f9bb552bf1018193a0278c60f2114fe17
GalexMP <alexmedina907@gmail.com>
2020-02-14 10:41:03 +0100

    modified DynamoDBDsl file, and add empty file CopteroArgumentsParser

--- Dsl/DynamoDBDsl.py
+++ Dsl/DynamoDBDsl.py
@@ -1,20 +1,28 @@
+from botocore.exceptions import ClientError
 from pyspark.sql import *
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
+
 # Get the service resource.
-dynamodb = boto3.resource('dynamodb')
-client = boto3.client('dynamodb')
+# dynamodb = boto3.resource('dynamodb', region_name='eu-central-1', endpoint_url="https://dynamodb.eu-central-1.amazonaws.com")
+# client = boto3.client('dynamodb')
+
 
 class DynamoDBDsl:
 
-
     def getAuxTablePath(auxTableName):
+        dynamodb = boto3.resource('dynamodb', region_name='eu-central-1',
+                                  endpoint_url="https://dynamodb.eu-central-1.amazonaws.com")
         table = dynamodb.Table("coptero_state")
-        key_to_get = {}
-        key_to_get.update({'process', Attr('rod_master_table')})
-        key_to_get.update({'module', Attr(auxTableName)})
-        response = table.get_item(Key=key_to_get)
-        # items = response['items']
-        # return items
-        return response['info']
+        try:
+            response = table.get_item(
+                Key={
+                    'process': 'rod_master_table',
+                    'module': auxTableName
+                }
+            )
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            return response['Item']['info']
