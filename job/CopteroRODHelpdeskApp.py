3b94f9e426f6c0f3edf57414100fa179f3f87df1
GalexMP <alexmedina907@gmail.com>
2020-02-19 10:38:26 +0100

    indentation

--- job/CopteroRODHelpdeskApp.py
+++ job/CopteroRODHelpdeskApp.py
@@ -8,33 +8,29 @@ import argparse
 
 class CopteroRODHelpdeskApp:
 
+    def main(self):
+        AppName = "Coptero ROD Helpdesk 5min Continuous Batch"
 
- def main(self):
-    AppName = "Coptero ROD Helpdesk 5min Continuous Batch"
+        # Construct the argument parser
+        ap = argparse.ArgumentParser()
 
+        # Add the arguments to the parser
+        ap.add_argument("-c", "--conf", required=True,
+                        help="s3 conf file path")
+        ap.add_argument("-f", "--file", required=True,
+                        help="s3 ingest file path")
+        args = vars(ap.parse_args())
 
+        s3confPath = str(args['conf'])
+        s3filePath = str(args['file'])
 
-    # Construct the argument parser
-    ap = argparse.ArgumentParser()
+        if any(s3filePath):
+            conf = SparkConf()
+            conf.setAppName(AppName)
+            sc = SparkContext(conf=conf)
+            CopteroRODHelpdeskJob.runJob(sc, s3confPath, s3filePath)
+        else:
+            sys.exit(1)
 
-    # Add the arguments to the parser
-    ap.add_argument("-c", "--conf", required=True,
-                    help="s3 conf file path")
-    ap.add_argument("-f", "--file", required=True,
-                    help="s3 ingest file path")
-    args = vars(ap.parse_args())
-
-    s3confPath = str(args['conf'])
-    s3filePath = str(args['file'])
-
-    if any(s3filePath):
-        conf = SparkConf()
-        conf.setAppName(AppName)
-        sc = SparkContext(conf=conf)
-        CopteroRODHelpdeskJob.runJob(sc, s3confPath, s3filePath)
-    else:
-        sys.exit(1)
-
-
- if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
