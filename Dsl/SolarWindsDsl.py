f4de5e5d26125f49f9ce833164b7301b7748c9a1
gonzalo.serrano.gonzalez <gonzalo.serrano.gonzalez@alumnos.upm.es>
2020-03-19 11:40:22 +0100

    Solarwinds new version 1.1

--- Dsl/SolarWindsDsl.py
+++ Dsl/SolarWindsDsl.py
@@ -1,12 +1,15 @@
-'''
-package tbs.bigdata.dsl
+from pyspark.sql import *
+import pyspark.sql.functions as F
+from Dsl.S3FilesDsl import S3FilesDsl
+from model.AdminNumberTags import AdminNumberTags
+from utils.Utils import Utils
 
-import scala.language.implicitConversions
+class SolarWindsDsl():
+    def adminNumberTags(tagsAdminPath):
+        rodTicketANTags =  AdminNumberTags.antagsColumns(S3FilesDsl.readFile())
 
-object SolarWindsDsl {
-
-  //sql server
-
-}
-
-'''
+        ticketANTags = rodTicketANTags \
+            .groupBy("admin_number") \
+            .agg(F.concat_ws(",", F.collect_set("tags").alias("tags"))) \
+            .withColumn("tags", Utils.stringToArray("tags"))
+        return ticketANTags
