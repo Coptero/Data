5402418ae9c5ea6882d3627d0ce3c38d37493f35
GalexMP <alexmedina907@gmail.com>
2020-03-19 11:37:59 +0100

    configjson modified, added new attributes

--- config/ConfigJson.py
+++ config/ConfigJson.py
@@ -6,6 +6,8 @@ class ConfigJson(object):
     operational_path: str
     tags_admin_path: str
     tags_operating_path: str
+    customer_path: str
+    end_customer_path: str
     fast_min_results: int
     fast_postgresql: str
     fast_user: str
@@ -15,11 +17,21 @@ class ConfigJson(object):
     fast_sc_query: str
     fast_agg_query: str
     fast_nni_query: str
+    fast_crm_query: str
+    fast_supplier_query: str
+    fast_ispwire_query: str
     fast_parquet_path: str
     fast_main_parquet_path: str
     fast_sc_parquet_path: str
     fast_agg_parquet_path: str
     fast_nni_parquet_path: str
+    fast_crm_parquet_path: str
+    fast_supplier_parquet_path: str
+    fast_ispwire_parquet_path: str
+    fast_network_parquet_path: str
+    rod_agent_smc_parquet_path: str
+    rod_relations_parquet_path: str
+    elastic_env_index_prefix: str
     elastic_nodes: str
     elastic_port: str
     elastic_user: str
