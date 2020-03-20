3698300a2328c2d7c4f37d6ef095f179518d29f0
gonzalo.serrano.gonzalez <gonzalo.serrano.gonzalez@alumnos.upm.es>
2020-03-20 11:35:34 +0100

    CopteroRODWorkinfoJob new version 1.1

--- job/CopteroRODWorkinfoJob.py
+++ job/CopteroRODWorkinfoJob.py
@@ -1,14 +1,15 @@
 import sys
-sys.path.append("C:/Users/mou_i/Desktop/Python/LabCoptero/")
+
+sys.path.append("C:/Users/gonza/Downloads/LABSpark/")
 from model import TicketDetailHelpdesk
-from model.LogESIndex import LogESIndex, startLogStatus, logESindexSchema
+from model.LogESIndex import LogESIndex
 from Dsl.S3FilesDsl import S3FilesDsl
 from Dsl.ValidationsDsl import ValidationsDsl
 from Dsl.ElasticDsl import ElasticDsl
 from Dsl.AlertsDsl import AlertDsl
 from pyspark.sql import *
-#from pyspark.sql import SparkSession
-#from pyspark import SparkContext
+# from pyspark.sql import SparkSession
+# from pyspark import SparkContext
 import logging
 from datetime import datetime
 from model.TicketWorkInfo import getWISchema, workInfoHPDColumns
@@ -22,54 +23,56 @@ from utils.Utils import Utils
 class CopteroRODWorkinfoJob(SparkJob):
     def runJob(sparkSession, s3confPath, s3filePath):
         spark = sparkSession
-        logStatus = startLogStatus(s3filePath)
+        logStatus = LogESIndex.startLogStatus(s3filePath)
         dfCount = 0
         try:
             logging.info(
                 "Start batch Coptero ROD for s3confPath: " + s3confPath + "--------------------------------------")
             validatedRecords = ValidationsDsl.validateTickets(s3filePath,
-                                                              S3FilesDsl.readFileSchema(s3filePath, getWISchema(s3filePath), spark),
-                                                              spark)
+                                                              S3FilesDsl.readFileSchema(s3filePath,
+                                                                                        getWISchema(s3filePath), spark,
+                                                                                        s3confPath),
+                                                              spark,
+                                                              s3confPath)
             # Necesitamos ? rodTicketWorkinfoHelpdesk = workInfoHPDColumns(validatedRecords)
 
-            partitioned = validatedRecords.\
-                withColumn("ticket_max_value_partition", Utils.getIndexPartition("ticket_id")).\
-                withColumn("file", F.lit("s3filePath"))
+            indexAgentSmcCluster = RemedyDsl.getAgentSmcCluster(validatedRecords, s3confPath)
 
+            indexWithRelations = RemedyDsl.getRelations(indexAgentSmcCluster, s3confPath)
+
+            partitioned = indexWithRelations \
+                .withColumn("ticket_max_value_partition", Utils.getIndexPartition("ticket_id")) \
+                .withColumn("file", F.lit("s3filePath")) \
+                .withColumn("work_info_category", Utils.getWorkInfoCategory("work_info_notes"))
+
+            dfCount = partitioned.count()
             logging.info("Persisting ES index..")
-            #dfCount = esIndex.count().toInt
-            #logging.info("indexDataFrame.count.." + dfCount)
+
+            logging.info("indexWorkInfoDataFrame.count().." + dfCount)
             try:
-                ElasticDsl.writeMappedESIndex(partitioned, "copt-rod-wif-{ticket_max_value_partition}", "instanceid")
+                ElasticDsl.writeMappedESIndex(partitioned, "copt-rod-wif-{ticket_max_value_partition}", "instanceid",
+                                              s3confPath)
             except Exception as e:
-                message = str(e)
-                if message.find("index_closed_exception"):
+                if e.getMessage.contains("index_closed_exception"):
                     raise e
                 else:
                     # TODO saveToEs {partitioned} works fine but ends with exception ?¿
-                    logging.info("catched index_closed_exception: " + str(e))
+                    logging.info("catched index_closed_exception: " + e.getMessage)
 
             # AlertDsl.checkCount("copt-rod-closed-*", s3filePath, dfCount,spark)
 
             logStatus = copy.deepcopy(logStatus)
             logStatus.success = True
-            logStatus.count = dfCount
-            logStatus.exception = ""
-            logStatus.end_date = ""
+            # logStatus.count = dfCount
             logging.info("End batch Coptero ROD ----------------------------------------------------")
         except Exception as e:
             logStatus = copy.deepcopy(logStatus)
             logStatus.success = False
-            logStatus.count = dfCount
-            logStatus.exception = str(e)
-            logStatus.end_date = ""
-            logging.error("catched: " + str(e))
+            logStatus.exception = e.getMessage
+            logging.error("catched: " + e.getMessage)
             raise e
         finally:
             sqlContext = SQLContext(spark)
-            logStatus.end_date = datetime.now().strftime("%Y%m%d%H%M%S")
-            logStatus_data = logESindexSchema(logStatus.file, logStatus.count, logStatus.success, logStatus.exception,
-                                              logStatus.start_date, logStatus.end_date)
-            logDataFrame = sqlContext.createDataFrame(copy.deepcopy(logStatus_data))
-            logDataFrame.show(5)
-            ElasticDsl.writeESLogIndex(logDataFrame, "copt-rod-log-")
+            logDataFrame = sqlContext.createDataFrame(copy.deepcopy(logStatus))
+            logDataFrame.end_date = datetime.now().strftime("%Y%m%d%H%M%S")
+            ElasticDsl.writeESLogIndex(logDataFrame, "copt-rod-log-", s3confPath)
