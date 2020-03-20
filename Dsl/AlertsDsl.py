94d45e00589ced52cc160fae77ce80bfc6e9a1e0
gonzalo.serrano.gonzalez <gonzalo.serrano.gonzalez@alumnos.upm.es>
2020-03-20 10:21:28 +0100

    AlertsDsl new version 1.1

--- Dsl/AlertsDsl.py
+++ Dsl/AlertsDsl.py
@@ -1,30 +1,31 @@
-# Faltan imports
 import sys
-sys.path.append("C:/Users/mou_i/Desktop/Python/LabCoptero/") 
+
+sys.path.append("C:/Users/gonza/Downloads/Data-master/")
 import logging
 from pyspark.sql import *
 from datetime import datetime
 from pyspark import *
-from model.Alert import Alert
-from elasticsearch import Elasticsearch
+
 from Dsl.ElasticDsl import ElasticDsl
 
 
-class AlertDsl():
-    # Falta el implicit spark SparkSession
-    def checkCount(indexName, fileName, dfCount, spark):
+class AlertDsl:
+    def checkCount(indexName, fileName, dfCount, spark, conf):
         logging.info('dfCount.. ' + dfCount)
         path = fileName.replace(':', r'\\:').replace("/", r"\\/")
         qResultDF = spark.sqlContext.read \
             .option("es.resource", "indexName") \
-            .option("es.query", r"?q=file:\'' + path + '\'' ") \
+            .option("es.query", r"?q=file:\''" + path + " '\'") \
             .format("org.elasticsearch.spark.sql") \
             .load()
         # ¿Equivalente de qResultDF = spark.esDF("${indexName}", "?q=file:\"" + path + "\"").select("ticket_id") ?
-        qResultDF.cache
-        queryCount = qResultDF.count
-        qResultDF.unpersist
+        qResultDF.cache()
+        queryCount = qResultDF.count()
+        qResultDF.unpersist()
         logging.info("queryCount.. " + queryCount)
         if dfCount != queryCount:
-            alertDataFrame = spark.createDataFrame([Alert(fileName, dfCount, queryCount, datetime.now().strftime("%Y%m%d%H%M%S"))])
-            ElasticDsl.writeESAlertsIndex(alertDataFrame)
+            alertDataFrame = spark.createDataFrame(
+                [(fileName, dfCount, queryCount, datetime.now().strftime("%Y%m%d%H%M%S"))],
+                ["file", "expected_count", "result_count", "date"])
+            ElasticDsl.writeESAlertsIndex(alertDataFrame, conf)
+
