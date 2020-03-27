de965f3a96e7700291deb061b41c76fa96b24af6
gonzalo.serrano.gonzalez <gonzalo.serrano.gonzalez@alumnos.upm.es>
2020-03-27 09:40:18 +0100

    TicketToClose without class

--- model/TicketToClose.py
+++ model/TicketToClose.py
@@ -1,20 +1,19 @@
-from dataclasses import dataclass
 from pyspark.sql import *  # DataFrame, SparkSession
 from pyspark.sql.types import StringType, StructField, StructType
 
 
-class TicketToClose:
-    def getClosedSchema(spark):
-        validationSchema = StructType([
-            StructField("ticket_id", StringType(), True),
-            StructField("status_id", StringType(), True),
-            StructField("_corrupt_record", StringType(), True)
-        ])
+def getClosedSchema(spark):
+    validationSchema = StructType([
+        StructField("ticket_id", StringType(), True),
+        StructField("status_id", StringType(), True),
+        StructField("_corrupt_record", StringType(), True)
+    ])
 
-        return validationSchema
+    return validationSchema
 
-    def detailClosedColumns(file, spark):
-        AdminNumberTags = ['ticket_id', 'status_id']
-        for c, n in zip(file.columns, AdminNumberTags):
-            file = file.withColumnRenamed(c, n)
-        return file
+
+def detailClosedColumns(file, spark):
+    AdminNumberTags = ['ticket_id', 'status_id']
+    for c, n in zip(file.columns, AdminNumberTags):
+        file = file.withColumnRenamed(c, n)
+    return file
