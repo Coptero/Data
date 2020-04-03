b50c77e075d12e48fe888e5835a78ee3ade5f468
GalexMP <alexmedina907@gmail.com>
2020-04-03 13:15:33 +0200

    Utils and Network files modified

--- model/Network.py
+++ model/Network.py
@@ -1,13 +1,10 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Network(object):
-    pop: str
-    router: str
-    interface: str  # interface is a reserved keyword and cannot be used as field name FOR SPARK-EXPLODE
-    sub_interface: str
-    customer: str
-    end_customer: str
-    vendor: str
-    access_type: str
+class Network:
+    def __init__(self, pop, router, interface, sub_interface, customer, end_customer, vendor, access_type):
+        self.pop = pop
+        self.router =  router
+        self.interface = interface  # interface is a reserved keyword and cannot be used as field name FOR SPARK-EXPLODE
+        self.sub_interface = sub_interface
+        self.customer = customer
+        self.end_customer = end_customer
+        self.vendor = vendor
+        self.access_type = access_type
