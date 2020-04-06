285947057e90e44cfdf25dbaac5259f3292a5af7
GalexMP <alexmedina907@gmail.com>
2020-04-06 10:24:53 +0200

    files network and utils modified

--- model/Network.py
+++ model/Network.py
@@ -1,10 +1,10 @@
 class Network:
-    def __init__(self, pop, router, interface, sub_interface, customer, end_customer, vendor, access_type):
+    def __init__(self, pop, router, access_type, end_customer, sub_interface, vendor, interface, customer):
         self.pop = pop
-        self.router =  router
-        self.interface = interface  # interface is a reserved keyword and cannot be used as field name FOR SPARK-EXPLODE
-        self.sub_interface = sub_interface
-        self.customer = customer
-        self.end_customer = end_customer
-        self.vendor = vendor
+        self.router = router
         self.access_type = access_type
+        self.end_customer = end_customer
+        self.sub_interface = sub_interface
+        self.vendor = vendor
+        self.interface = interface
+        self.customer = customer
